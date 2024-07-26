e732c46a2de67bbb46e9777df338bc7ca010f29d
Idriss EL Abidi <136001812+Idriss-Abidi@users.noreply.github.com>
2024-07-26 12:42:10 +0100

    commit

---
+++ models/fact/fact_all_BA.sql
@@ -0,0 +1,34 @@
+WITH bonanalyse_data AS (
+              SELECT DISTINCT
+                  b."idBonAnalyse", 
+                  b."numeroBA",
+                  b."dateCreation",
+                  b.active,
+                  b."montantInitial",
+                  b."idProfesseur",
+                  b.professeur,
+                  me.email,
+                  me.cin,
+                  me.pays,
+                  me.faculte_ecole,
+                  me.EtablissementUniversitaire,
+                  b.statut ,
+                  b.statut_confirmation,
+                  
+                  b.laboratoire,
+                --   dlu."idUniteMesure",
+                  b.unitemesure,
+                --   dtu."idTypeAnalyse",
+                  b.typeAnalyse,
+                --   dtu."idUniteMesure",
+                  b.typeAnalyse_unitemesure
+                --   dtu."valeur"
+              FROM 
+                  {{ ref('dim_bonanalyse') }} b
+              INNER JOIN 
+                  {{ ref('dim_professeur') }} me ON b."idProfesseur" = me."id"
+                order by  b."dateCreation" 
+              
+          )
+          SELECT * FROM bonanalyse_data
+          
---
+++ models/fact/fact_all_devis.sql
@@ -0,0 +1,32 @@
+WITH devis_data AS (
+              SELECT DISTINCT
+                  d."idDevis",
+                  d."numeroDevis", 
+                  d."dateCreation",
+                  d."active", 
+                  d."montantInitial",
+                  d."consomme",
+                  d."idParticulier",
+                  me.Particulier,
+                  me."cin", 
+                  me."email",
+                  
+                  d.laboratoire,
+                --   dlu."idUniteMesure",
+                  d.unitemesure,
+                --   dtu."idTypeAnalyse",
+                  d.typeAnalyse,
+                --   dtu."idUniteMesure",
+                  d.typeAnalyse_unitemesure
+                --   dtu."valeur"
+
+              FROM 
+                  {{ ref('dim_devis') }} d
+              INNER JOIN 
+                  {{ ref('dim_particulier') }} me ON d."idParticulier" = me."idMembreExterne"
+              
+            where active=TRUE 
+            order by  d."dateCreation"
+
+          )
+          SELECT * FROM devis_data
